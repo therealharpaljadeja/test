a4544f30dd87f51b2dc015792d7f27f7aaa33306
Harpal Jadeja <jadejaharpal14@gmail.com>
2021-03-22 16:41:49 +0530

    added add function

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,4 +3,8 @@ class HelloWorld{
   public static void main(String []args){
     System.out.println("Hello World");
   }
+ 
+  public static void add(int num1,int num2){
+      System.out.println(num1+num2);
+  }
 }
